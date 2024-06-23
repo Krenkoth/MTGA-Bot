baef0daac1b02f4b4f67ad5daa5fd28e058f0016
John Skelton <john.g.skelton21@gmail.com>
2024-06-23 07:41:26 -0700

    Main now parses MTGA log file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 MTGA Bot to play a lot of ranked games automatically
 
 Currently working on playing Mono R Cavalcade from left to right
+
+Need to start parser after starting Arena
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyautogui as pag
 import time
-from mtga.set_data import all_mtga_cards
+# from mtga.set_data import all_mtga_cards
 
 def queue_recent():
     pag.moveTo(1740, 1000)
@@ -13,11 +13,32 @@ def queue_recent():
     time.sleep(0.1)
     pag.mouseUp()
 
-# print(all_mtga_cards.find_one(66819))
+def follow(file):
+    # print(file.tell())
+    file.seek(0, 2)
+    while True:
+        line = file.readline()
+        if not line:
+            continue
+        yield line
 
+
+# print(all_mtga_cards.find_one(66819))
+f = open(
+    "C:/Users/johng/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log"
+)
+print(f.readline())
+# line = f.readline()
+log = follow(f)
+print(log.__next__())
 while True:
-    file_path = 'C:/Users/johng/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log'
-    with open(file_path, 'r') as file:
-        lines = file.read().splitlines()
-        print(len(lines))
+    # try:
+    line = log.__next__()
+    print(line)
+    # except S
+# while True:
+    # file_path = 'C:/Users/johng/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log'
+    # with open(file_path, 'r') as file:
+    #     lines = file.read().splitlines()
+    #     print(len(lines))
 
