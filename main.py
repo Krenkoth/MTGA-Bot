24de5b2a31fe8cea6b24a9b2f0b54791fe95d08e
John Skelton <john.g.skelton21@gmail.com>
2024-06-22 21:16:40 -0700

    Added examples of in-game actions to txt, copied code from GPT for reading log file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyautogui as pag
 import time
+from mtga.set_data import all_mtga_cards
 
 def queue_recent():
     pag.moveTo(1740, 1000)
@@ -12,5 +13,11 @@ def queue_recent():
     time.sleep(0.1)
     pag.mouseUp()
 
+# print(all_mtga_cards.find_one(66819))
 
-queue_recent()
+while True:
+    file_path = 'C:/Users/johng/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log'
+    with open(file_path, 'r') as file:
+        lines = file.read().splitlines()
+        print(len(lines))
+
