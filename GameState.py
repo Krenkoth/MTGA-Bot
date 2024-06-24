5c6b1c215f621e46f8b9991f21be6e9f54fb92e5
John Skelton <john.g.skelton21@gmail.com>
2024-06-23 21:01:59 -0700

    changes to gamestate.py

--- GameState.py
+++ GameState.py
@@ -1,3 +1,39 @@
+
+
+# phases are: begin, main1, main2, combat, oppTurn
+# boards and hand are lists of permanent objects
+
 class GameState:
     def __init__(self):
         self.phase = "pregame"
+        self.hand = []
+        self.landDrop = True
+        self.myBoard = []
+        self.oppBoard = []
+
+    def drawCard(self, id):
+        self.hand.append(id)
+    
+    def goTo(self, phase):
+        self.phase = phase
+        if phase == "begin":
+            self.landDrop = True
+            for perm in self.myBoard:
+                perm.untap()
+        if phase == "oppTurn":
+            for perm in self.oppBoard:   
+                perm.untap()
+                
+    def playedLand(self):
+        self.landDrop = False
+
+class permanent:
+    def __init__(self, id):
+        self.id = id
+        self.untapped = True
+    
+    def tap(self):
+        self.untapped = False
+
+    def untap(self):
+        self.untapped = True
