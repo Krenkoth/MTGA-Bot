b869da4f9de7ac57d750bed93a855dad0f6237ae
John Skelton <john.g.skelton21@gmail.com>
2024-06-25 20:31:49 -0700

    testing

--- Testing.py
+++ Testing.py
@@ -1,4 +1,13 @@
 import pyautogui as pag
+# f = open(
+#     "C:/Users/johng/AppData/LocalLow/Wizards Of The Coast/MTGA/Player.log"
+# )
+# log = follow(f)
 
+# while True:
+    
 while True:
     print(pag.position())
+
+    # 300, 1070
+    # 160, 1070
